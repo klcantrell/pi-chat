b31ecd815891cfa3e2374f6be5674a566f9bc306
Kalalau <cantrellkalalau@gmail.com>
2018-08-30 15:49:35 -0400

    messing with dropdown component

--- front-end/src/app/components/Test.js
+++ front-end/src/app/components/Test.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
-import 'materialize-css/js/slider';
+import 'materialize-css/js/dropdown';
 import style from '../../style/components/test.scss';
 
 class Test extends Component {
+  componentDidMount() {
+    const elems = document.querySelectorAll('.dropdown-trigger');
+    const instances = M.Dropdown.init(elems, { coverTrigger: false });
+  }
   
   render() {
     return (
-      <>
+      <div style={{width: "300px"}} ref={node => this.container = node}>
         <h1 className={style.header}>sup!</h1>
         <a className="waves-effect waves-light btn">button</a>
-      </>
+        <a className='dropdown-trigger btn' data-target='dropdown1'>Drop Me!</a>
+
+        <ul id='dropdown1' className={`dropdown-content ${style["dropdown-width"]}`}>
+          <li><span>one</span></li>
+          <li><span>two</span></li>
+          <li className="divider" tabIndex="-1"></li>
+          <li><span>three</span></li>
+          <li><span><i className="material-icons">view_module</i>four</span></li>
+          <li><span><i className="material-icons">cloud</i>five</span></li>
+        </ul>
+      </div>
     );
   }
 }
