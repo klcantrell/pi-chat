a5eaefeeb6cb13127a8cb19faad8a304987ffe34
Kalalau <cantrellkalalau@gmail.com>
2018-09-05 22:03:12 -0400

    setup and test of cognito and appsync.  need to work out cognito issues.

--- front-end/src/app/index.js
+++ front-end/src/app/index.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import AWSAppSyncClient from 'aws-appsync';
+import Amplify, { Auth } from 'aws-amplify';
+import { Provider } from 'react-apollo';
+import gql from 'graphql-tag';
+
 import 'materialize-css/js/cash';
 // import 'materialize-css/js/component'; // this is now being imported in each component module I bring in
 import 'materialize-css/js/global';
@@ -8,8 +13,41 @@ import 'materialize-css/js/waves';
 import 'materialize-css/js/forms';
 import '../style/app.global.scss';
 
+import awsConfig from './aws.config';
 import App from './components/App';
 
+Amplify.configure({
+  Auth: {
+    identityPoolId: awsConfig.identityPoolId,
+    region: awsConfig.region,
+  },
+});
+
+
+const client = new AWSAppSyncClient({
+  url: awsConfig.graphqlEndpoint,
+  region: awsConfig.region,
+  auth: {
+    type: awsConfig.authenticationType,
+    credentials: () => Auth.currentCredentials(),
+  },
+});
+
+client.query({
+  query: gql`
+    {
+      singleChat {
+        items {
+          id
+          message
+          createdAt
+        }
+      }
+    }
+  `,
+})
+  .then(res => console.log(res.data))
+  .catch(err => console.log(err));
 
 ReactDOM.render(
   <App />,
